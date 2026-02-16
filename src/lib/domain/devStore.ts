c7bb7e20608bd9df268301b4167db6e050002466
David <dcljlong@gmail.com>
2026-02-16 21:24:26 +1300

    feat: add TEMP dev in-memory store (M1)

---
+++ src/lib/domain/devStore.ts
@@ -0,0 +1,10 @@
+﻿/*
+  TEMP DEV DATA STORE
+  This will be replaced by Supabase in M8.
+*/
+
+import { Job, DayEntry, Item } from '@/types/domain';
+
+export const jobs: Job[] = [];
+export const dayEntries: DayEntry[] = [];
+export const items: Item[] = [];
